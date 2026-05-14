15b6417f8b892146212f4fc213688295fefb1caf
Thailan Swaity <th4lans@gmail.com>
2026-05-13 20:09:20 -0500

    Commands for playing music in vlc

--- thai.zsh
+++ thai.zsh
@@ -10,7 +10,7 @@ function vif() {
 }
 
 function rec() {
-	cd $(tac $CDHIST | awk '!seen[$0]++' | fzy)
+	cd "$(tac $CDHIST | awk '!seen[$0]++' | fzy)"
 }
 
 function gg() {
@@ -35,7 +35,6 @@ function open() {
 	local SESSIONNAME="${FILENAME//./_}"
 
 	if [ -n "$TMUX" ]; then
-		echo "In a tmux session"
 		tmux new -A -s "$SESSIONNAME" -d -- "vim $FILENAME";
 		tmux switch-client -t "$SESSIONNAME"
 	else
@@ -51,4 +50,64 @@ function tmrec() {
 	else
 		tmux attach -t "$SESSIONNAME"
 	fi
+}
+
+function vibe() {
+	cvlc "$HOME/Music/" --random --loop
+}
+
+function playlist() {
+	tmux new -s "vlc" -d -- "vlc --intf rc --rc-host 127.0.0.1:44500";
+}
+
+function quit_playlist() {
+	echo "quit" | nc -N 127.0.0.1 44500
+}
+
+function add_songf() {
+	echo "add  $(fzf --walker-root=$HOME/Music --walker=dir,file,follow,hidden)" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function q_songf() {
+	echo "enqueue  $(fzf --walker-root=$HOME/Music --walker=dir,file,follow,hidden)" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function add_song() {
+	echo "add $1" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function q_song() {
+	echo "enqueue $1" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function next() {
+	echo "next" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function prev() {
+	echo "prev" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function play() {
+	echo "play" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function pause() {
+	echo "pause" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function stop() {
+	echo "stop" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function volume() {
+	echo "volume $1" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function shuffle() {
+	echo "random" | nc -N 127.0.0.1 44500 >/dev/null 2>&1
+}
+
+function status() {
+	echo "status" | nc -N 127.0.0.1 44500
 }
